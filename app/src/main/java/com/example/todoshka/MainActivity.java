00965a28596a087f4960f4749920c9ccca0b8f83
Kamran Aghayev <aghayev.kamran@outlook.com>
2023-05-25 05:23:21 +0400

    Test

---
+++ app/src/main/java/com/example/todoshka/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.todoshka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
